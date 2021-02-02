b13461ac87e2ec5a994d63f2833173f22716de2c
Yaser <yaser.azem@otmail.com>
2021-02-02 22:09:37 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
